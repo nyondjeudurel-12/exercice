7851fb3335d694a7303d2b11eb53553d83c0349e
Daiki ny <nyondjeudurel8.com>
2026-09-19 11:06:27 +0100

    modification

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 int main() {
     std::string name;
-    std::cout << "Enter your name: ";
+    std::cout << "Enter your name:";
     std::getline(std::cin, name);
     std::cout << "Hello, " << name << "!" << std::endl;
     return 0;
