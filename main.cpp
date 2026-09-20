b63f749c71748cde0562a12aee64f2a2788e95ef
Daiki ny <nyondjeudurel8.com>
2026-09-20 11:00:19 +0100

    autre modification- ajout du deuxieme commentaire

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 int main() {
     //modification premiere du fichier
+    //autre modification du fichier
 
     std::string name;
     std::cout << "Enter your name:";
